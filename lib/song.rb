829c8209dde54fa21d98705e37071084c7fbd9b7
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-29 15:53:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -61,7 +61,7 @@ class Song
     filename[1].slice!(".mp3")
     name = filename[1]
     artist_name = filename[0]
-    song.find_or_create_by_name(name)
+    song = self.find_or_create_by_name(name)
     song.artist_name = artist_name
     song
   end
